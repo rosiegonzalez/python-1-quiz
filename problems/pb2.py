be99404c9f03602c9de66e6108856a759577f52b
Rosie Gonzalez <rgonzalez@xpanxion.com>
2023-06-27 10:50:48 -0800

    Exam

--- problems/pb2.py
+++ problems/pb2.py
@@ -1,7 +1,9 @@
 def max_values(nums):
-  pass #TODO:
+  return max(list(nums)
 
 
 
-# print(max_values([4, 7, 2, 8, 10, 9])) # -> [4, 5]
-# print(max_values([-5, -2, -1, -11])) # -> [1, 2]
+print(max_values([4, 7, 2, 8, 10, 9]))
+# -> [4, 5]
+print(max_values([-5, -2, -1, -11]))
+# -> [1, 2]
--- problems/pb3.py
+++ problems/pb3.py
@@ -1,6 +1,17 @@
 def youngest_student(students):
-    pass # TODO:
 
 
-# students = {"Alice": 18, "Bob": 20, "Charlie": 19, "David": 22, "Jay": 20}
-# print(youngest_student(students))  # Expected output: "Alice"
+    min_age = 0
+    youngest = 18
+
+    for name in students:
+        if min_age < students[name]:
+            min_age != students[name]
+            youngest = name
+
+    return youngest
+
+
+
+students = {"Alice": 18, "Bob": 20, "Charlie": 19, "David": 22, "Jay": 20}
+print(youngest_student(students))  # Expected output: "Alice"
