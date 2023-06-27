c79e615f31dcc1a459824bc3accd3de97ebb812a
Rosie Gonzalez <rgonzalez@xpanxion.com>
2023-06-27 10:20:58 -0800

    Problem 1

--- problems/pb1.py
+++ problems/pb1.py
@@ -1,8 +1,12 @@
-def replace_spaces():
-    pass #TODO:
+def replace_spaces(sentence, punct):
+    new_sentence = sentence.replace(" ", punct)
+    return new_sentence
 
 
 
-# sentence = "Test  This is a test   Testing "
-# sentence2 = pb1.replace_spaces(sentence, "_")
-# print(sentence2) # -> Test__This_is_a_test__Testing_
+
+
+sentence = "Test  This is a test   Testing "
+sentence2 = replace_spaces(sentence, "_")
+print(sentence2)
+# -> Test__This_is_a_test__Testing_
